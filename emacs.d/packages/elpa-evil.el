cc861b6e2ff82267ae1b1237ac5fcc26a1f2121f
Héctor Menéndez <etor@gik.mx>
2017-09-11 07:27:40 -0500

    Simplified order and improved evil.

--- emacs.d/packages/elpa-evil.el
+++ emacs.d/packages/elpa-evil.el
@@ -1,31 +1,13 @@
-;; VI rocks! there, I said it.
+;;; elpa-evil.el --- Enable Vim emulation on Emacs.
+
+;;; Commentary:
+
+;;; Code:
+
 (use-package evil
     :ensure t
     :config (progn
-        ;; Enable tpope's vim-commentary port
-        (use-package evil-commentary
-            :ensure t
-            :delight evil-commentary-mode
-        )
-        ;; Enable tpope's vim-surround port (globally)
-        (use-package evil-surround
-            :ensure t
-            :delight evil-surround-mode
-            :config (global-evil-surround-mode 1)
-        )
-        ;; Enable the <leader> key like in Vim
-        (use-package evil-leader
-            :ensure t
-            :config (global-evil-leader-mode 1)
-        )
-        ;; Enable comentary mode only on programming mode
-        (add-hook 'prog-mode-hook 'evil-commentary-mode)
-        ;; To avoid issues load these after emacs has initialized
-        (add-hook 'evil-mode-hook '(lambda ()
-            (evil-leader/set-leader "SPC")
-            (evil-leader/set-key "?" 'which-key-show-top-level)
-            (evil-leader/set-key "y" 'copy-region-as-kill); copy selection
-            (evil-leader/set-key "p" 'yank-rectangle); paste selection
+        (add-hook 'evil-local-mode-hook (lambda ()
             ;; Have <tab> to work as it does on Vim
             (define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
             (define-key evil-motion-state-map (kbd "C-b") nil); scroll down
@@ -37,4 +19,33 @@
     )
 )
 
+; ;; Enable the <leader> key like in Vim
+(use-package evil-leader
+    :ensure t
+    :config (add-hook 'evil-local-mode-hook (lambda ()
+        (evil-leader-mode 1)
+        (evil-leader/set-leader "SPC")
+        (evil-leader/set-key "?" 'which-key-show-top-level)
+        (evil-leader/set-key "y" 'copy-region-as-kill); copy selection
+        (evil-leader/set-key "p" 'yank-rectangle); paste selection
+    ))
+)
+;; Enable tpope's vim-commentary port
+(use-package evil-commentary
+    :ensure t
+    :delight evil-commentary-mode
+    :config (add-hook 'prog-mode-hook 'evil-commentary-mode)
+)
+
+;; Enable tpope's vim-surround port (globally)
+(use-package evil-surround
+    :ensure t
+    :delight evil-surround-mode
+    :config (add-hook 'evil-local-mode-hook (lambda ()
+        (evil-surround-mode 1)
+    ))
+)
+
+
 (provide 'elpa-evil)
+;;; elpa-evil.el ends here
