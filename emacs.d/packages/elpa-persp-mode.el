be757bea58ccfde17ea6dc07f6971fa0c0966e2d
Héctor Menéndez <etor@gik.mx>
2017-09-13 03:46:58 -0500

    Better integrarated persp-mode (bindings)

--- emacs.d/packages/elpa-persp-mode.el
+++ emacs.d/packages/elpa-persp-mode.el
@@ -3,38 +3,34 @@
     :ensure t
     :delight persp-mode
     :config (progn
+
+        (add-hook 'evil-leader-mode-hook (lambda ()
+            (evil-leader/set-key "TAB" 'persp-switch);; quick perspective switch
+        ))
+
+        (add-hook 'after-init-hook (lambda ()
+            (define-key global-map (kbd "M-B") 'persp-kill-buffer)
+            ;; always kill buffer using persp-mode
+            (substitute-key-definition 'kill-buffer 'persp-kill-buffer global-map)
+            ;; Load the last-saved perspective state
+            ;; NOTE: The persp-auto-resume-time, not always work, this does.
+            (persp-load-state-from-file
+                (expand-file-name persp-auto-save-fname persp-save-dir)
+            )
+        ))
+
         (setq
             persp-autokill-buffer-on-remove 'kill ; kill the buffer when closed
             persp-nil-name "nil"; The name of the default perspective
-            persp-save-dir "~/.emacs.d/_perspectives/"
-            persp-auto-save-fname "autosave"
+            persp-save-dir (expand-file-name "_perspectives/" user-emacs-directory)
+            persp-auto-save-fname "persp-autosave"
             persp-set-last-persp-for-new-frames nil; don't use last persp for new frames
+            persp-switch-to-added-buffer nil; don't switch window to new buffers
             persp-auto-save-opt 1; Auto-save perspective on buffer kill
-            ;; persp-auto-resume-time 1; Load perspectives on startup
+            persp-auto-resume-time -1; Don't auto resume perspective on startup
             persp-nil-hidden t; hide the nil perspective
         )
-        ;; Integrate with projectile
-        (use-package persp-mode-projectile-bridge
-            :ensure t
-            :config (add-hook 'persp-mode-projectile-mode-hook '(lambda ()
-                (if persp-mode-projectile-bridge-mode
-                    (persp-mode-projectile-bridge-find-perspectives-for-all-buffers)
-                    (persp-mode-projectile-bridge-kill-perspectives)
-                )
-            ))
-        )
-        (add-hook 'evil-local-mode-hook '(lambda ()
-            (evil-leader/set-key "TAB" 'persp-switch);; quick perspective switch
-            (global-set-key (kbd "C-b") nil); backward_char
-            (global-set-key (kbd "C-b") 'persp-switch-to-buffer)
-            (global-set-key (kbd "C-B") 'persp-kill-buffer)
-            (global-set-key (kbd "M-w") nil); kill-ring-save
-            (global-set-key (kbd "M-w") 'kill-this-buffer)
-        ))
-        (with-eval-after-load "persp-mode"
-            ;; always kill buffer using persp-mode
-            (substitute-key-definition 'kill-buffer 'persp-kill-buffer global-map)
-        )
+
         (persp-mode 1)
     )
 )
