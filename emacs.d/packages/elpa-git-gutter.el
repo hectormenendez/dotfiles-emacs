bb36627323ef75dddf030b01ebf77ce50d268b4b
Héctor Menéndez <etor@gik.mx>
2017-09-12 23:23:00 -0500

    git-gutter now runs only in prog-mode. Added new update-hooks

--- emacs.d/packages/elpa-git-gutter.el
+++ emacs.d/packages/elpa-git-gutter.el
@@ -1,16 +1,26 @@
-;; Adds a gutter with the git status of each file (duh)
+;;; elpa-git-gitter.el --- Adds a gutter with the git status of each line
+
+;;; Commentary:
+
+;;; Code:
 (use-package git-gutter
     :ensure t
     :delight git-gutter-mode
-    :config (progn
-        (setq git-gutter:update-interval 2); Update git gutter after n secs idle
-        (setq git-gutter:ask-p nil); Don't ask confirmation when committing or reverting
-        (add-to-list 'git-gutter:update-hooks 'focus-in-hook)
-        (add-to-list 'git-gutter:update-hooks 'magit-post-refresh-hook)
+    :config (add-hook 'prog-mode-hook (lambda ()
+        (setq
+            git-gutter:update-interval 2; Update git gutter after n secs idle
+            git-gutter:ask-p nil; Don't ask confirmation when committing or reverting
+        )
+        (add-to-list 'git-gutter:update-hooks 'git-gutter-mode-hook)
         (add-to-list 'git-gutter:update-hooks 'git-gutter:post-command-hook)
+        (add-to-list 'git-gutter:update-hooks 'focus-in-hook)
+        (add-to-list 'git-gutter:update-hooks 'magit-post-display-buffer-hook)
+        (add-to-list 'git-gutter:update-hooks 'magit-post-refresh-hook)
+        (add-to-list 'git-gutter:update-hooks 'after-save-hook)
         (git-gutter:linum-setup); play along with linum-mode
-        (git-gutter-mode 1)
-    )
+        (global-git-gutter-mode 1)
+    ))
 )
 
 (provide 'elpa-git-gutter)
+;;; elpa-git-gutter.el ends here
