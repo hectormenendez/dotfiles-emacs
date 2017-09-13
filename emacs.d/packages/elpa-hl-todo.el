f88ded59385c8b7a68156bc7cc447c1cf5806067
Héctor Menéndez <etor@gik.mx>
2017-09-13 03:44:19 -0500

    Commented out hl-todo bindings for later defining.

--- emacs.d/packages/elpa-hl-todo.el
+++ emacs.d/packages/elpa-hl-todo.el
@@ -5,13 +5,16 @@
 ;;; Code:
 (use-package hl-todo
     :ensure t
-    :bind (
-        ("C-c t n" . hl-todo-next)
-        ("C-c t p" . hl-todo-previous)
-        ("C-c C-t" . hl-todo-occur)
-    )
     :config (add-hook 'prog-mode-hook (lambda ()
         (hl-todo-mode 1)
+
+        ;; TODO: Set evil leader bindings for this.
+
+        ;; :bind (
+        ;;     ("C-c t n" . hl-todo-next)
+        ;;     ("C-c t p" . hl-todo-previous)
+        ;;     ("C-c C-t" . hl-todo-occur)
+        ;; )
     ))
 )
 
