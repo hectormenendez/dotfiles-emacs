d1721bd0399d9d8d01bdb9f50df38304c90bf4f0
Héctor Menéndez <etor@gik.mx>
2017-10-05 08:34:20 -0500

    customized company-mode behaviour.

--- emacs.d/packages/elpa-company.el
+++ emacs.d/packages/elpa-company.el
@@ -2,7 +2,18 @@
 (use-package company
     :ensure t
     :delight company-mode
-    :config (add-hook 'after-init-hook 'global-company-mode)
+    :config (progn
+        (setq
+            ;; Whenever you type two letters show a possible completion
+            company-idle-delay 0
+            company-minimum-prefix-length 2
+
+            company-auto-complete nil; Don't automatically auto-complete.
+            company-tooltip-limit 20; The max number of candidates to show
+            company-tooltip-align-annotations t; Align  annotations to the right side
+        )
+        (add-hook 'after-init-hook 'global-company-mode)
+    )
 )
 
 (provide 'elpa-company)
