ceef30d5ecae4a8b91ee0fb454ebae68dfd06dcd
Héctor Menéndez <etor@gik.mx>
2017-10-11 14:41:25 -0500

    Refactored js2-mode settings for better indentation support.

--- emacs.d/packages/elpa-js2-mode.el
+++ emacs.d/packages/elpa-js2-mode.el
@@ -4,46 +4,37 @@
 
 ;;; Code:
 
-;; Enable tern
 (use-package tern
+    ;; Enable tern
     :ensure t
     :config (add-hook 'js2-mode-hook (lambda ()
+
         (setq tern-flash-timeout 30); Wait 30 seconds before removing the function helper
         (tern-mode 1)
     ))
 )
 
-;; Add tern to copany backends.
 (use-package company-tern
+    ;; Add tern to copany backends.
     :ensure t
     :after tern
-    :config (add-hook 'company-mode-hook (lambda ()
+    :config (add-hook 'js2-mode-hook (lambda ()
         (add-to-list 'company-backends 'company-tern)
     ))
 )
 
+(use-package js2-highlight-vars
+    :ensure t
+    :after js2-mode
+)
+
 (use-package js2-mode
     :ensure t
+    :after company-tern
     :interpreter "node"
     :mode (("\\.js\\'" . js2-mode))
-    :after company-tern
-    :config (add-hook 'js2-mode-hook (lambda ()
-        ;; Indent lines using this function.
-        (setq-local indent-line-function 'js2-indent-line)
-        (setq
-            js2-basic-offset 4
-            js2-highlight-level 3
-            js2-include-browser-externs t
-            js2-include-node-externs t
-            js2-warn-about-unused-function-arguments t
-            ;; let flycheck handle parse errors
-            js2-mode-show-parse-errors nil
-            js2-strict-missing-semi-warning nil
-            js2-strict-trailing-comma-warning nil
-            js2-strict-cond-assign-warning nil
-            js2-strict-inconsistent-return-warning nil
-            js2-strict-var-hides-function-arg-warning nil
-            js2-strict-var-redeclaration-warning nil
+    :config (progn
+        (add-hook 'flycheck-mode-hook (lambda () (setq-default
             ;; Enable only eslint on flycheck
             flycheck-disabled-checkers (append '(
                 javascript-jshint
@@ -53,8 +44,29 @@
             flycheck-enabled-checkers (append '(
                 javascript-eslint
             ))
-        )
-    ))
+        )))
+        (add-hook 'js2-mode-hook (lambda ()
+            (setq
+                js2-highlight-level 3; Add syntax highlighting to everything
+                js2-include-browser-externs t
+                js2-include-node-externs t
+                js2-mode-show-overlay t
+
+                ;; let flycheck handle warnings and errors
+                js2-mode-show-parse-errors nil
+                js2-mode-show-strict-warnings nil
+                js2-warn-about-unused-function-arguments t
+            )
+
+            ;; I don't follow strict rules for indentation so
+            ;; until I find a better way to customize the indentation I'll settle with this.
+            (electric-indent-local-mode -1)
+            (setq-local indent-line-function 'indent-relative-maybe)
+
+            ;; Highlight matching vars
+            (js2-highlight-vars-mode 1)
+        ))
+    )
 )
 
 (use-package rjsx-mode
@@ -73,4 +85,3 @@
 
 (provide 'elpa-js2-mode)
 ;;; elpa-js2-mode.el ends here
-
