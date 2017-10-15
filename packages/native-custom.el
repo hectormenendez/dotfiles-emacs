571cc801b75218698310fea7e33e0cc8885f764f
Héctor Menéndez <hector@gik.mx>
2017-10-15 07:59:30 -0500

    native-custom: default files/dir validation & creation.

--- packages/native-custom.el
+++ packages/native-custom.el
@@ -2,13 +2,28 @@
 (use-package custom
     ;; TODO: validate the existence of the theme before trying to load it
     :demand t
+    :init (defvar etor/themes-dir
+        (expand-file-name "_themes" user-emacs-directory)
+    )
     :config (progn
         (setq
             custom-safe-themes t; Don't ask for confirmations when loading themes
-            custom-file "~/.emacs.d/_custom.el"; Save customisations to this file
+            ;; Save customisations to this file
+            custom-file (expand-file-name "_custom.el" user-emacs-directory)
         )
-        (add-to-list 'custom-theme-load-path "~/.emacs.d/_themes/"); Themes location
+
+        ;; If the theme folder doesn't exist, create it and then enable it.
+        (unless
+            (file-exists-p etor/themes-dir)
+            (make-directory etor/themes-dir)
+        )
+        (add-to-list 'custom-theme-load-path etor/themes-dir)
+
+        ;; if the custom file doesn't exist, create it.
+        (unless (file-exists-p custom-file) (write-region "" nil custom-file))
         (load custom-file)
+
+
     )
 )
 
