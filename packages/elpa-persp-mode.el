116eaf4d8340271e5833562901eb31310da5bbe6
Héctor Menéndez <hmenendez@techlap.local>
2017-10-23 09:04:27 -0500

    persp-mode: Updated settings for latest build.

--- packages/elpa-persp-mode.el
+++ packages/elpa-persp-mode.el
@@ -6,12 +6,12 @@
 
         (add-hook 'evil-leader-mode-hook (lambda ()
             (evil-leader/set-key "TAB" 'persp-switch);; quick perspective switch
+            ;; always kill buffer using persp-mode
+            (define-key global-map (kbd "M-B") 'persp-kill-buffer)
+            (substitute-key-definition 'kill-buffer 'persp-kill-buffer global-map)
         ))
 
         (add-hook 'after-init-hook (lambda ()
-            (define-key global-map (kbd "M-B") 'persp-kill-buffer)
-            ;; always kill buffer using persp-mode
-            (substitute-key-definition 'kill-buffer 'persp-kill-buffer global-map)
             ;; Load the last-saved perspective state
             ;; NOTE: The persp-auto-resume-time, not always work, this does.
             (persp-load-state-from-file
@@ -21,18 +21,21 @@
 
         (setq
             persp-autokill-buffer-on-remove 'kill ; kill the buffer when closed
-            persp-nil-name "nil"; The name of the default perspective
+            persp-kill-foreign-buffer-behaviour 'kill ; don't ask for confirmation
+            persp-nil-name "none"; The name of the default perspective
             persp-save-dir (expand-file-name "_perspectives/" user-emacs-directory)
             persp-auto-save-fname "persp-autosave"
-            persp-set-last-persp-for-new-frames nil; don't use last persp for new frames
+            persp-set-last-persp-for-new-frames t
             persp-switch-to-added-buffer nil; don't switch window to new buffers
             persp-auto-save-opt 1; Auto-save perspective on buffer kill
             persp-auto-resume-time -1; Don't auto resume perspective on startup
-            persp-nil-hidden t; hide the nil perspective
+            persp-nil-hidden nil; set to true to hide the "nil" perspective
+            persp-remove-buffers-from-nil-persp-behaviour 'kill
         )
 
         (persp-mode 1)
     )
 )
+
 
 (provide 'elpa-persp-mode)
