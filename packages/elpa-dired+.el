c0df00ca36441b3e2e1ce8080064a7ac9d75ad7b
Héctor Menéndez <hmenendez@techlap.local>
2018-09-19 12:34:57 -0300

    Added keybindings to diredmode
    - M-¢ to open iTerm on current directory
    - "G" and "gg" to navigate dired mode using evil keybindings

--- packages/elpa-dired+.el
+++ packages/elpa-dired+.el
@@ -2,16 +2,25 @@
 (use-package dired+
     :quelpa (dired+ :fetcher url :url "https://www.emacswiki.org/emacs/download/dired+.el")
     :config (progn
+
+        ;; Open iTerm in current directory using a key
+        (global-set-key (kbd "M-¢") (lambda () (interactive)
+            (dired-smart-shell-command "open -a iTerm $PWD" nil nil)
+        ))
+
+        ;; Add jump to current file's folder
         (add-hook 'evil-local-mode-hook (lambda ()
-            ;; Add jump to current file's folder
             (global-set-key (kbd "C-x C-j") nil);original binding
             (evil-leader/set-key "." 'dired-jump)
         ))
-        ;;Use evil on dired mode
+
         (eval-after-load 'dired (lambda ()
+            ;;Use evil on dired mode
             (add-hook 'evil-local-mode-hook (lambda ()
                 (evil-make-overriding-map dired-mode-map 'normal t); the standard bindings
                 (evil-define-key 'normal dired-mode-map
+                    "gg" 'evil-goto-first-line
+                    "G" 'evil-goto-line
                     "h" 'evil-backward-char
                     "j" 'evil-next-line
                     "k" 'evil-previous-line
