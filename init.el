19645cd76f48706970bdc67c8ce2efb9cbb96286
Héctor Menéndez <hector@gik.mx>
2019-04-09 23:25:33 -0500

    orginit: Evil mode setup on wrappers, added macrostep for Elisp

--- init.el
+++ init.el
@@ -7,15 +7,24 @@
 
 ;; ------------------------------------------------------------------------- Core settings
 
+(defvar etor/init:flag "_DELME-TO-UPDATE.flag")
+(defvar etor/init:index "README.org")
+
 (defvar etor/path (expand-file-name (file-name-as-directory "sections") user-emacs-directory))
-(defvar etor/path:index "README.org")
-(defvar etor/path:bundled (concat etor/path (file-name-as-directory "bundled")))
-(defvar etor/path:bundled-editor (concat etor/path:bundled (file-name-as-directory "editor")))
-(defvar etor/path:bundled-content (concat etor/path:bundled (file-name-as-directory "content")))
 (defvar etor/path:system (concat etor/path (file-name-as-directory "system")))
+(defvar etor/path:custom (concat etor/path (file-name-as-directory "custom")))
 (defvar etor/path:editor (concat etor/path (file-name-as-directory "editor")))
 (defvar etor/path:content (concat etor/path (file-name-as-directory "content")))
-(defvar etor/path:deltofetch (expand-file-name "_deltofetch" user-emacs-directory))
+
+(defvar etor/path:editor-bundled (concat etor/path:editor (file-name-as-directory "bundled")))
+(defvar etor/path:editor-package (concat etor/path:editor (file-name-as-directory "package")))
+(defvar etor/path:editor-package-visual (concat etor/path:editor-package (file-name-as-directory "visual")))
+(defvar etor/path:editor-package-commands (concat etor/path:editor-package (file-name-as-directory "commands")))
+(defvar etor/path:editor-package-wrappers (concat etor/path:editor-package (file-name-as-directory "wrappers")))
+(defvar etor/path:editor-package-navigation (concat etor/path:editor-package (file-name-as-directory "navigation")))
+
+(defvar etor/path:content-bundled (concat etor/path:content (file-name-as-directory "bundled")))
+(defvar etor/path:content-package  (concat etor/path:content (file-name-as-directory "package")))
 
 ; set GC to something big to optimize loading time, restore it after load.
 (setq gc-cons-threshold 64000000)
@@ -30,16 +39,24 @@
     message-log-max 10000; max number of lines to keep in logfile.
     load-prefer-newer t; Don't load outdated byte code
     max-mini-window-height 0.5; Use up to 50% of frame height for mini-buffer window.
+    ;; TODO: this should be set only for GUI
     line-spacing 1; The spacing between lines vertically
-    tab-width 4; Use 4 spaces for indentation
-    fill-column 90; word-wrap after the 90th character
 )
 
 ;; Startup.el
-;; This file parses the command line and gets Emacs running.
+;; Basic settings setup while loading Emacs.
 (setq
     inhibit-startup-screen 1; Don't show the welcome screen
     initial-scratch-message nil; Don't show a message on *scratch* mode
+    ; the location for auto-save files
+    auto-save-list-file-prefix
+        (expand-file-name (file-name-as-directory "_auto-save-list") user-emacs-directory)
+)
+
+;; TODO: These should be set on custom.el
+(setq-default
+    tab-width 4; Use 4 spaces for indentation
+    fill-column 90; word-wrap after the 90th character
 )
 
 ; Ask for just one letter when confirmation needed
@@ -67,7 +84,7 @@
 ;; if quelpa not present, download and install it so packages can be compiled from source.
 ;; Also, install quelpa-use-package so those packages can be loaded using "use-package"
 ;; (use-package is a dependecy so it won't be necessary to explicitly install it)
-(unless (file-exists-p etor/path:deltofetch) (progn
+(unless (file-exists-p (expand-file-name etor/init:flag user-emacs-directory)) (progn
     ; make sure packages are up-to-date
     (package-refresh-contents)
     ; Install quelpa for additional packages
@@ -86,7 +103,7 @@
         :fetcher git
         :url "https://framagit.org/steckerhalter/quelpa-use-package.git")
     ))
-    (write-region "" nil etor/path:deltofetch)
+    (write-region "" nil (expand-file-name etor/init:flag user-emacs-directory))
 )
 
 ;; initialize use-package with quelpa support
@@ -105,6 +122,7 @@
     :config
     (setq
         org-hide-leading-stars nil; Always show stars in headings
+        org-startup-folded "children"
     )
 )
 
@@ -117,10 +135,27 @@
 ;; )
 
 ;; Load the initialization
-(org-babel-load-file (expand-file-name etor/path:index user-emacs-directory))
+(org-babel-load-file (expand-file-name etor/init:index user-emacs-directory))
 
 ;; ---------------------------------------------------------------------------------- DONE
-(message (concat "Emacs took " (emacs-init-time) " to load."))
+(defun display-startup-echo-area-message ()
+    "Overwritten startup message."
+    (message (concat "Emacs took " (emacs-init-time) " to load."))
+)
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(restart-emacs try toc-org rainbow-mode quelpa-use-package org-plus-contrib delight))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
