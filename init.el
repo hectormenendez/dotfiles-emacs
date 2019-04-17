efa6c85fac05bf51ae91633f05abdb9eed3a27b5
Héctor Menéndez <hector@gik.mx>
2019-04-17 12:22:54 -0500

    orginit: WIP, generating an orgfile from scratch.

--- init.el
+++ init.el
@@ -1,34 +1,64 @@
-;;; init.el --- Hector Menendez' Emacs configuration.
+;;; init.el -- Emacs initialization.
+;;; -*- lexical-binding: t; -*-
 
 ;;; Commentary:
-;;; This is the main entry point for my Emacs's config, it loads README.org afterwards.
+;;; This is weird.
 
 ;;; Code:
 
-;; ------------------------------------------------------------------------- Core settings
+;; ------------------------------------------------------------------ Minimal Vars & Funcs
+;; These are the minimal custom variables and functions needed to startup the config.
 
 (defvar etor/init:flag "_DELME-TO-UPDATE.flag")
 (defvar etor/init:index "README.org")
+(defvar etor/slash (substring user-emacs-directory -1)); The path separator
 
-(defvar etor/path (expand-file-name (file-name-as-directory "sections") user-emacs-directory))
-(defvar etor/path:system (concat etor/path (file-name-as-directory "system")))
-(defvar etor/path:custom (concat etor/path (file-name-as-directory "custom")))
-(defvar etor/path:editor (concat etor/path (file-name-as-directory "editor")))
-(defvar etor/path:content (concat etor/path (file-name-as-directory "content")))
+;; Path management
+;; Contains a list of paths available to get using etor/path:get
+(defvar etor/path:list '())
 
-(defvar etor/path:editor-bundled (concat etor/path:editor (file-name-as-directory "bundled")))
-(defvar etor/path:editor-package (concat etor/path:editor (file-name-as-directory "package")))
-(defvar etor/path:editor-package-commands (concat etor/path:editor-package (file-name-as-directory "commands")))
-(defvar etor/path:editor-package-majormodes (concat etor/path:editor-package (file-name-as-directory "majormodes")))
-(defvar etor/path:editor-package-visual (concat etor/path:editor-package (file-name-as-directory "visual")))
-(defvar etor/path:editor-package-navigation (concat etor/path:editor-package (file-name-as-directory "navigation")))
+(defun etor/path:get (key)
+    "Return a path declared on the etor/paths list.  (KEY)."
+    (interactive)
+    (cdr (assoc key etor/path:list))
+)
+(defun etor/path:put (key val)
+    "Set a new path to the etor/paths list.  (KEY, VAL)."
+    (interactive)
+    (let ((parts (split-string val ":")))
+        (if (= (length parts) 1)
+            ;; an identifier couldn't be found, insert the predicate as is.
+            (push (cons key (file-name-as-directory val)) etor/path:list)
+            (let*
+                (
+                    (target (etor/path:get (car parts)))
+                    (route (file-name-as-directory (mapconcat 'identity (cdr parts) "/")))
+                )
+                (push (cons key (concat target route)) etor/path:list)
+            )
+        )
+    )
+)
 
-(defvar etor/path:content-bundled (concat etor/path:content (file-name-as-directory "bundled")))
-(defvar etor/path:content-package  (concat etor/path:content (file-name-as-directory "package")))
+(defun etor/path:print ()
+    "Output to Messages the contents of the path variable."
+    (interactive)
+    (let (( list etor/path:list ))
+        (while
+            (print (car list))
+            (setq list (cdr list))
+        )
+    )
+)
 
-; set GC to something big to optimize loading time, restore it after load.
-(setq gc-cons-threshold 64000000)
-(add-hook 'after-init-hook #'(lambda () (setq gc-cons-threshold 800000)))
+(etor/path:put "emacs" (expand-file-name user-emacs-directory))
+(etor/path:put "autosave" "emacs:_autosave")
+(etor/path:put "sections" "emacs:sections")
+(etor/path:put "packages" "emacs:_packages")
+(etor/path:put "packages-elpa" "packages:_melpa")
+(etor/path:put "packages-quelpa" "packages:_quelpa")
+
+;; ------------------------------------------------------------------------------- Startup
 
 ;; C Source code
 ;; these are the absolute first thing that should be setup.
@@ -39,40 +69,36 @@
     message-log-max 10000; max number of lines to keep in logfile.
     load-prefer-newer t; Don't load outdated byte code
     max-mini-window-height 0.5; Use up to 50% of frame height for mini-buffer window.
-    ;; TODO: this should be set only for GUI
-    line-spacing 1; The spacing between lines vertically
 )
 
-;; Startup.el
+;; startup.el
 ;; Basic settings setup while loading Emacs.
 (setq
     inhibit-startup-screen 1; Don't show the welcome screen
     initial-scratch-message nil; Don't show a message on *scratch* mode
     ; the location for auto-save files
-    auto-save-list-file-prefix
-        (expand-file-name (file-name-as-directory "_auto-save-list") user-emacs-directory)
+    auto-save-list-file-prefix (etor/path:get "autosave")
 )
 
 ;; TODO: These should be set on custom.el
 (setq-default
     tab-width 4; Use 4 spaces for indentation
     fill-column 90; word-wrap after the 90th character
+    line-spacing 1; The spacing between lines vertically
 )
 
 ; Ask for just one letter when confirmation needed
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; ----------------------------------------------------------------------- Package Manager
+;; -------------------------------------------------------------------- Package Management
 
 (require 'package)
 
 (setq
     package-enable-at-startup nil; Disable the default packaage-manager at startup
-    package-user-dir (expand-file-name "_elpa" user-emacs-directory); packages dir
+    package-user-dir (etor/path:get "packages-elpa")
 )
-(setq-default
-    quelpa-dir (expand-file-name "_quelpa" user-emacs-directory)
-)
+(setq-default quelpa-dir (etor/path:get "packages-quelpa"))
 
 ;; The repositories to fetch packages-from.
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -84,58 +110,43 @@
 ;; if quelpa not present, download and install it so packages can be compiled from source.
 ;; Also, install quelpa-use-package so those packages can be loaded using "use-package"
 ;; (use-package is a dependecy so it won't be necessary to explicitly install it)
-(unless (file-exists-p (expand-file-name etor/init:flag user-emacs-directory)) (progn
-    ; make sure packages are up-to-date
-    (package-refresh-contents)
-    ; Install quelpa for additional packages
-    (if (require 'quelpa nil t)
-        ;; only upgrade if already fetched
-        (quelpa-self-upgrade)
-        ;; not installed, fetch and install.
-        (with-temp-buffer
-            (url-insert-file-contents
-                "https://framagit.org/steckerhalter/quelpa/raw/master/bootstrap.el")
-            (eval-buffer)
+
+(let ((file (concat (etor/path:get "emacs") etor/init:flag)))
+    (unless (file-exists-p file) (progn
+        ; make sure packages are up-to-date
+        (package-refresh-contents)
+        ; Install quelpa for additional packages
+        (if (require 'quelpa nil t)
+            ;; only upgrade if already fetched
+            (quelpa-self-upgrade)
+            ;; not installed, fetch and install.
+            (with-temp-buffer
+                (url-insert-file-contents
+                    "https://framagit.org/steckerhalter/quelpa/raw/master/bootstrap.el")
+                (eval-buffer)
+            )
         )
-    )
-    ;; Add use-package with quelpa support
-    (quelpa '(quelpa-use-package
-        :fetcher git
-        :url "https://framagit.org/steckerhalter/quelpa-use-package.git")
+        ;; Add use-package with quelpa support
+        (quelpa '(quelpa-use-package
+            :fetcher git
+            :url "https://framagit.org/steckerhalter/quelpa-use-package.git"
+        ))
+        ;; Write file so this doesn't run again until the file is deleted
+        (write-region "" nil file)
     ))
-    (write-region "" nil (expand-file-name etor/init:flag user-emacs-directory))
 )
 
 ;; initialize use-package with quelpa support
 (require 'quelpa-use-package)
 
-(use-package bind-key :demand t); Enables :bind (key binding) on use-package
+(use-package bind-key :ensure t); Enables :bind (key binding) on use-package
 (use-package delight :ensure t); Enables :delight (custom mode name) on use-package
 
-;; ------------------------------------------------------------------------------ ORG Mode
+;; ------------------------------------------------------------------------------ ORG MODE
+;; Use orgmode to load the rest of the configuration for a better reading experience.
 
-;; Use the latest contrib version of org.
-;; TODO: maybe use defer instead of demand? does org-babel-load-file need this?
-(use-package org
-    :ensure org-plus-contrib
-    :demand t
-    :config
-    (setq
-        org-hide-leading-stars nil; Always show stars in headings
-        org-startup-folded "children"
-    )
-)
-
-;; TODO: This is not working for whatever reason.
-;; Allows the generation of a Table of Contents for Github
-;; (use-package toc-org
-;;     :ensure t
-;;     :hook org-mode
-;;     :commands toc-org-mode
-;; )
-
-;; Load the initialization
-(org-babel-load-file (expand-file-name etor/init:index user-emacs-directory))
+;; The rest of the configuration will be loaded using literate elisp.
+;; (org-babel-load-file (concat (etor/path:get "emacs") etor/init:index))
 
 ;; ---------------------------------------------------------------------------------- DONE
 (defun display-startup-echo-area-message ()
@@ -145,17 +156,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-	(restart-emacs try toc-org rainbow-mode quelpa-use-package org-plus-contrib delight))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
